389dfb06f7ab0b2115a0e43182c6bf4d5d6aa36a
danielalt2 <64434303+danielalt2@users.noreply.github.com>
2022-04-14 14:16:37 -0600

    Create stickyDesktopImages.js

---
+++ gatsby/stickyDesktopImages.js
@@ -0,0 +1,56 @@
+function checkWidth() {
+
+try{
+
+  var gW = window.innerWidth;
+
+  if (gW < 768){
+    notDesktop();
+  }
+
+  if (gW >= 768){
+    desktop();
+  }
+}
+catch(e){}
+}
+
+
+
+function desktop(){
+try{
+  var photoArea = document.querySelectorAll("[class^='PdpApp']")[0];
+  photoArea = photoArea.firstChild.firstChild;
+  photoArea.style.position = "sticky";
+  photoArea.style.height = "100%";
+  photoArea.style.top = "0";
+
+}
+catch(e){
+
+}
+}
+
+function notDesktop(){
+try{
+  var photoArea = document.querySelectorAll("[class^='PdpApp']")[0];
+  photoArea = photoArea.firstChild.firstChild;
+  photoArea.style.position = "relative";
+  photoArea.style.height = "auto";
+  photoArea.style.top = "";
+
+}
+catch(e){
+
+}
+}
+
+try{
+checkWidth();
+window.addEventListener("resize", checkWidth);  
+}
+catch(e){}
+
+
+
+
