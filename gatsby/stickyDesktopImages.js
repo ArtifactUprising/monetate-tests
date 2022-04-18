5b13b9a7a857b66832d5242cfbef0b38258f5d7e
Nick Graziano <nick.graziano@gmail.com>
2022-04-18 09:28:59 -0600

    Formatting

--- gatsby/stickyDesktopImages.js
+++ gatsby/stickyDesktopImages.js
@@ -1,56 +1,41 @@
 function checkWidth() {
+  try {
+    const gW = window.innerWidth;
 
-try{
+    if (gW < 768) {
+      notDesktop();
+    }
 
-  var gW = window.innerWidth;
-
-  if (gW < 768){
-    notDesktop();
-  }
-
-  if (gW >= 768){
-    desktop();
-  }
-}
-catch(e){}
+    if (gW >= 768) {
+      desktop();
+    }
+  } catch (e) {}
 }
 
 
+function desktop() {
+  try {
+    let photoArea = document.querySelectorAll("[class^='PdpApp']")[0];
+    photoArea = photoArea.firstChild.firstChild;
+    photoArea.style.position = "sticky";
+    photoArea.style.height = "100%";
+    photoArea.style.top = "0";
 
-function desktop(){
-try{
-  var photoArea = document.querySelectorAll("[class^='PdpApp']")[0];
-  photoArea = photoArea.firstChild.firstChild;
-  photoArea.style.position = "sticky";
-  photoArea.style.height = "100%";
-  photoArea.style.top = "0";
-
-}
-catch(e){
-
-}
+  } catch (e) {}
 }
 
-function notDesktop(){
-try{
-  var photoArea = document.querySelectorAll("[class^='PdpApp']")[0];
-  photoArea = photoArea.firstChild.firstChild;
-  photoArea.style.position = "relative";
-  photoArea.style.height = "auto";
-  photoArea.style.top = "";
+function notDesktop() {
+  try {
+    let photoArea = document.querySelectorAll("[class^='PdpApp']")[0];
+    photoArea = photoArea.firstChild.firstChild;
+    photoArea.style.position = "relative";
+    photoArea.style.height = "auto";
+    photoArea.style.top = "";
 
-}
-catch(e){
-
-}
+  } catch (e) {}
 }
 
-try{
-checkWidth();
-window.addEventListener("resize", checkWidth);  
-}
-catch(e){}
-
-
-
-
+try {
+  checkWidth();
+  window.addEventListener("resize", checkWidth);
+} catch (e) {}
